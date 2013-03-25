393bc7ca5ab43cf696a0a2f3c7ec49535d53e1d8
Robert Lummis <robert.lummis@gmail.com>
2013-03-25 16:40:57 -0400

    initial commit. This is a revised version using only one cell class

---
+++ SPoT HW4/HW4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HW4AppDelegate.h
+//  SPoT HW4
+//
+//  Created by Robert Lummis on 3/11/13.
+//  Copyright (c) 2013 ElectricTurkeySoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HW4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPoT HW4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SPoT HW4
+//
+//  Created by Robert Lummis on 3/11/13.
+//  Copyright (c) 2013 ElectricTurkeySoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HW4AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HW4AppDelegate class]));
+    }
+}
